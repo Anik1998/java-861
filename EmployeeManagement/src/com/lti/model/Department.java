07c56154e677f3e5a85131907421bb960c021d4e
Anik <Anik@Anik>
2020-10-15 13:27:39 +0530

    add department

---
+++ EmployeeManagement/src/com/lti/model/Department.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Department {
+	int deparmentno;
+	int departmentname;
+	public int getDeparmentno() {
+		return deparmentno;
+	}
+	public void setDeparmentno(int deparmentno) {
+		this.deparmentno = deparmentno;
+	}
+	public int getDepartmentname() {
+		return departmentname;
+	}
+	public void setDepartmentname(int departmentname) {
+		this.departmentname = departmentname;
+	}
+	
+}
